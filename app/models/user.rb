64a26db8ab8420903613259b787b1c2fd2467e50
nariomasahisa <68809067+nariomasahisa@users.noreply.github.com>
2020-11-07 19:29:32 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+    validates :username
+    validates :email
+    validates :password
+    validates :phone_number
+    validates :prefecture_address
+    validates :city_address
+    validates :house_number
+  end
+
+  validates :email, format: { with: /\A\S+@\S+\.\S+\z/ }
+  validates :password, 
+    format: { with: /\A[a-zA-Z0-9]+\z/ },
+    length: { minimum: 8 }
+  validates :phone_number, format: { with: /\A\d{10,11}\z/ }
 end
