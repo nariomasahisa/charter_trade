3e495f23681824477079319265de69696beb65c3
nariomasahisa <68809067+nariomasahisa@users.noreply.github.com>
2020-11-09 11:05:37 +0900

    チャーター便発注画面へ遷移

--- app/controllers/charters_controller.rb
+++ app/controllers/charters_controller.rb
@@ -1,7 +1,12 @@
 class ChartersController < ApplicationController
   def index
+    @charters = Charter.all
   end
   def new
-    
+    @charter = Charter.new
+  end
+
+  def create
+    @charter = Charter.create(params)
   end
 end
