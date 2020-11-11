c37b2c039d462d9f6df59e6ccb9cbaa1193acfec
nariomasahisa <68809067+nariomasahisa@users.noreply.github.com>
2020-11-11 19:31:51 +0900

    sellコントローラー編集

---
+++ app/controllers/sells_controller.rb
@@ -0,0 +1,17 @@
+class SellsController < ApplicationController  
+  def create
+    @sell = Sell.create(sell_params)
+    if @sell.save
+      redirect_to charter_path(@sell.charter)
+    else
+      @charter = @sell.charter
+      @sell = @charter.sell
+      render "charters/show"
+    end
+  end
+
+  private
+  def sell_params
+    params.require(:sell).permit(:sell).merge(user_id: current_user.id, charter_id: params[:charter_id])
+  end
+end
