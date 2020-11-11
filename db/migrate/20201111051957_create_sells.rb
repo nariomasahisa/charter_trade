1596673e676747c1cc21a178a1a6b3f3a1e8cfd1
nariomasahisa <68809067+nariomasahisa@users.noreply.github.com>
2020-11-11 14:39:19 +0900

    sellテーブルのマイグレーションファイルの設定

--- db/migrate/20201111051957_create_sells.rb
+++ db/migrate/20201111051957_create_sells.rb
@@ -1,7 +1,9 @@
 class CreateSells < ActiveRecord::Migration[6.0]
   def change
     create_table :sells do |t|
-
+      t.integer     :sell_price, null: false
+      t.references  :user_id,    null: false
+      t.references  :charter_id, null: false
       t.timestamps
     end
   end
