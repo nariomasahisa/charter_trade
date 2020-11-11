7d9f334f212de595b20362d849eeeb985f9ff417
nariomasahisa <68809067+nariomasahisa@users.noreply.github.com>
2020-11-11 11:47:34 +0900

    aaa

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock "~> 3.14.1"
 set :application, "charter_trade"
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url, "git@example.com:nariomasahisa/charter_trade.git"
+set :repo_url, "git@github.com:nariomasahisa/charter_trade.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
