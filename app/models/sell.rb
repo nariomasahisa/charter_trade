b16f3ba8761028940e055eab2064e81e384e9d09
nariomasahisa <68809067+nariomasahisa@users.noreply.github.com>
2020-11-16 11:03:40 +0900

    価格投稿のバリデーション

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,6 +1,9 @@
 class Sell < ApplicationRecord
   # バリデーション
   validates :sell, presence: true
+  validates :sell,
+    numericality: { greater_than_or_equal_to: 5000, less_than_or_equal_to: 10000000 },
+    format: { with: /\A[0-9]+\z/ }
   # アソシエーション
   belongs_to :user
   belongs_to :charter
