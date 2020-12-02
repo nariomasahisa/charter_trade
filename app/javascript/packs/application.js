af371c51e266111a240d27373dfa09a884da8533
nariomasahisa <68809067+nariomasahisa@users.noreply.github.com>
2020-12-02 13:16:47 +0900

    修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('../modal')
 require('jquery')
 require('../scroll')
-require('../modal')
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
